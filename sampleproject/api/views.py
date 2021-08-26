accdfdc94e3cf6f8d8adf5901001aeb6bee990ca
Adithya Srinivas <adithya.bellamkonda@vannainfotech.com>
2021-08-26 14:38:34 +0530

    CORS and rest-api using (JWT)

--- sampleproject/api/views.py
+++ sampleproject/api/views.py
@@ -1,11 +1,11 @@
 
+import project1
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .serializers import ProjectSerializer
-from project1.models import Project
-
+from project1.models import Project, Review
 from api import serializers
 
 
@@ -28,7 +28,6 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
 def getProjects(request):
 
     projects = Project.objects.all()
@@ -41,6 +40,32 @@ def getProjects(request):
 def getProject(request, pk):
     project = Project.objects.get(id = pk)
     serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def projectVote(request, pk):
+    
+    project = Project.objects.get(id=pk)
+    
+    # getting the current user and data given in the post
+    user = request.user.profile
+    data = request.data
+    
+    # getting Review from models checking to get or create by owner of the review
+    # and project that user is giving review for
+    review , created = Review.objects.get_or_create(
+        owner = user,
+        project = project
+    )
+    
+    # retreving the value from post and assigning it to the review database 
+    review.value = data['value']
+    review.save()
+    project.getVoteCount # triggering  getVoteCount to update the votes
+    
+    serializer = ProjectSerializer(project, many = False)
     return Response(serializer.data)
 
 
--- sampleproject/sampleproject/settings.py
+++ sampleproject/sampleproject/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
@@ -47,11 +49,13 @@ INSTALLED_APPS = [
     # frameworks
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
